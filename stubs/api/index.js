4edf027d63bfc0323a76964b5055a211fd918a49
deluvoisss <dilyaranasvyzi@gmail.com>
2025-11-11 18:45:22 +0300

    FloroMate app

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,9 +1,3 @@
-const path = require('path');
 const router = require('express').Router();
-
-// Здесь укажите путь к вашему index.js
-router.get('/index.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../src/index.js'));
-});
 
 module.exports = router;
