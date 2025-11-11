55b05e774521037ce69b951e55fb72c9ff2b7daf
deluvoisss <dilyaranasvyzi@gmail.com>
2025-11-11 17:55:25 +0300

    new

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,3 +1,9 @@
+const path = require('path');
 const router = require('express').Router();
+
+// Здесь укажите путь к вашему index.js
+router.get('/index.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../src/index.js'));
+});
 
 module.exports = router;
